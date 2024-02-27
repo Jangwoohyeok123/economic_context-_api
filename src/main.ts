e205e6aa83e17181075244fe3a484d7d4c21edba
baaami <lgh121546@naver.com>
2024-02-27 22:11:02 +0900

    Auth MCS 생성 및 CORS 전체 허용 설정
    M: Module
    C: Controller
    S: Service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    credentials: false,
+  };
+
+  app.enableCors(corsOptions);
+
+  await app.listen(4000);
 }
 bootstrap();
