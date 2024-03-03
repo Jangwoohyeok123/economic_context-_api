a822286824b83af912c4b516f9f73f80a7377172
baaami <lgh121546@naver.com>
2024-03-03 17:09:13 +0900

    공통 define script 추가 (Fred API Category Enum)

--- src/api/common/define/index.ts
+++ src/api/common/define/index.ts
@@ -3,3 +3,10 @@ import { Users } from 'src/api/user/entity/user.entity';
 export interface UserRequest extends Request {
   user: Users;
 }
+
+export enum EconomicCategory {
+  Interest = 114,
+  Exchange = 94,
+  Consume = 9,
+  Production = 31,
+}
