856a22cf5a606333482d8a3505253fa03fcd5df0
baaami <lgh121546@naver.com>
2024-03-05 21:39:04 +0900

    REST API URL 내 동사 삭제 후 HTTP method로 대체

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
@@ -18,20 +19,20 @@ export class UserController {
     return this.userService.SelectOneById(userId);
   }
 
-  // 유저 관심 지표 추가
-  @Post('/favorite/add/:id')
-  addFavorite(@Param('id', ParseIntPipe) userId: number, @Body() data: any) {
-    return this.userService.addFavoriteOne(userId, data.indicatorId);
-  }
-
   // 유저 관심 지표 획득
-  @Get('/favorite/get/:id')
+  @Get('/favorite/:id')
   getFavoriteIndicators(@Param('id', ParseIntPipe) userId: number) {
     return this.userService.getFavoriteIndicators(userId);
   }
 
+  // 유저 관심 지표 추가
+  @Post('/favorite/:id')
+  addFavorite(@Param('id', ParseIntPipe) userId: number, @Body() data: any) {
+    return this.userService.addFavoriteOne(userId, data.indicatorId);
+  }
+
   // 유저 관심 지표 삭제
-  @Post('/favorite/delete/:id')
+  @Delete('/favorite/:id')
   deleteFavorite(@Param('id', ParseIntPipe) userId: number, @Body() data: any) {
     return this.userService.deleteFavoriteOne(userId, String(data.indicatorId));
   }
