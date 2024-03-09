d91a69d12e337f5869f4dac2d9c7cc2cdaf2bfd1
baaami <lgh121546@naver.com>
2024-03-09 16:02:18 +0900

    요청에 대한 method, url, status code, useragent, ip를 기록하도록 미들웨어 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './api/auth/auth.module';
@@ -12,6 +12,7 @@ import { Users } from './api/user/entity/user.entity';
 import { Indicators } from './api/indicator/entity/indicator.entity';
 import { Contexts } from './api/context/entity/context.entity';
 import { Journals } from './api/journal/entity/journal.entity';
+import { LoggingMiddleware } from './api/common/middleware/logging.middleware';
 
 @Module({
   imports: [
@@ -26,4 +27,8 @@ import { Journals } from './api/journal/entity/journal.entity';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
