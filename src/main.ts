2ad70fbb3a1919359f562310558c330abfa444c3
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-30 13:13:55 +0900

    refactor: dotenv main.ts 에 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config(); // 여기서 환경 변수를 로드합니다.
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
