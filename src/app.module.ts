ed037a10031fe564efc92cefbed95ccf90f7d702
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-30 19:35:00 +0900

    ConfigModule, Joi 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,8 @@ import { Indicators } from './api/indicator/entity/indicator.entity';
 import { Contexts } from './api/context/entity/context.entity';
 import { Journals } from './api/journal/entity/journal.entity';
 import { LoggingMiddleware } from './api/common/middleware/logging.middleware';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
 @Module({
   imports: [
@@ -21,6 +23,20 @@ import { LoggingMiddleware } from './api/common/middleware/logging.middleware';
     JournalModule,
     IndicatorModule,
     ContextModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET_KEY: Joi.string().required(),
+        NEXT_PUBLIC_FREDKEY: Joi.string().required(),
+        GOOGLE_CLIENT_ID: Joi.string().required(),
+        GOOGLE_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_REDIRECT_URI: Joi.string().uri().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(), // 비밀번호가 비어 있을 수 있음을 허용합니다.
+        DB_DATABASE: Joi.string().required(),
+        DB_HOST: Joi.string().hostname().required(),
+      }),
+      isGlobal: true,
+    }),
     TypeOrmModule.forFeature([Users, Indicators, Contexts, Journals]),
     TypeOrmModule.forRoot(ormconfig),
   ],
