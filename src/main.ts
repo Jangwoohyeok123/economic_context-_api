6c3bb65b78840f3f36c1ac88eef6302208b05d57
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-03-30 15:16:41 +0900

    dotenv 수정

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
+// 프로젝트 루트에 있는 .env 파일을 참조하도록 설정합니다.
+// 빌드 후에는 dist 디렉토리 내부에서 실행되므로, 상위 폴더로 경로를 설정해야 합니다.
+dotenv.config({
+  path: path.join(__dirname, '..', '.env'),
+});
 async function bootstrap() {
-  dotenv.config(); // 여기서 환경 변수를 로드합니다.
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
