167a5ec71e5a802fb79a545e09010bd400df738a
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-04-04 22:37:54 +0900

    refactor: db host env 로 변경

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-console.log(process.env.DB_USERNAME);
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
