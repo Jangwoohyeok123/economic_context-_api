79271749e9668d12d7cd0823b2b8f3c32a485fee
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-08-05 17:03:11 +0900

    refactor: favorite entity 수정
    - class 이름에 favorite 추가 (context에 저장될 entity와 헷갈림)

--- src/api/favorite/entity/favorite.entity.ts
+++ src/api/favorite/entity/favorite.entity.ts
@@ -5,7 +5,7 @@ import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
   @description: favorite을 따로 테이블을 만든 이유는 user table에서 처리할 경우 제1 정규형을 위배하기 때문이다.
 */
 @Entity({ schema: 'economic_context', name: 'favorite_news' })
-export class News {
+export class FavoriteNews {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -14,14 +14,8 @@ export class News {
   user: Users;
 }
 
-@Entity({ schema: 'economic_context', name: 'favorite_stocks' })
-export class Stock {
-  @PrimaryGeneratedColumn()
-  id: number;
-}
-
 @Entity({ schema: 'economic_context', name: 'favorite_indicators' })
-export class Indicator {
+export class FavoriteIndicator {
   @PrimaryGeneratedColumn()
   id: number;
 }
