8b393b77b80f63b24103aa43c6a08f9edcdaf3d5
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-08-10 17:22:58 +0900

    feat: tag table 과 context table 관계설정
    - ManyToMany 관계로 설정

--- src/api/context/entity/context.entity.ts
+++ src/api/context/entity/context.entity.ts
@@ -1,12 +1,19 @@
 import { CommonEntity } from 'src/api/common/entity/common.entity';
 import { Indicators } from 'src/api/indicator/entity/indicator.entity';
 import { Journals } from 'src/api/journal/entity/journal.entity';
+import { Tag } from 'src/api/tag/entity/tag.entity';
 import { Column, Entity, ManyToMany, OneToMany } from 'typeorm';
 
 @Entity({ schema: 'economic_context', name: 'contexts' })
 export class Context extends CommonEntity {
   @Column('varchar', { name: 'name', length: 100 })
   name: string;
+
+  @Column('varchar', { name: 'description', length: 100, nullable: true })
+  description: string;
+
+  @ManyToMany(() => Tag, (tag) => tag.contexts)
+  tags: Tag[];
 
   @OneToMany(() => Journals, (journal) => journal.context)
   journals: Journals[];
--- src/api/tag/entity/tag.entity.ts
+++ src/api/tag/entity/tag.entity.ts
@@ -1,11 +1,16 @@
 import { CommonEntity } from 'src/api/common/entity/common.entity';
+import { Context } from 'src/api/context/entity/context.entity';
 import { User } from 'src/api/user/entity/user.entity';
-import { Column, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToMany, ManyToOne } from 'typeorm';
 
+@Entity({ schema: 'economic_context', name: 'tags' })
 export class Tag extends CommonEntity {
   @Column('varchar', { name: 'name', length: 100 })
   name: string;
 
   @ManyToOne(() => User, (user) => user.tags)
   user: User;
+
+  @ManyToMany(() => Context, (context) => context.tags)
+  contexts: Context[];
 }
