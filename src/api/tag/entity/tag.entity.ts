59ad690616a4fb91532103c6665f896eb84be907
Jangwoohyeok123 <dkanvk1@gmail.com>
2024-08-17 15:50:17 +0900

    fix: tagEntity user와의 관계설정
    - @JoinColumn추가
    - Context와 Tags Jointable 삭제

--- src/api/tag/entity/tag.entity.ts
+++ src/api/tag/entity/tag.entity.ts
@@ -4,7 +4,7 @@ import { User } from 'src/api/user/entity/user.entity';
 import {
   Column,
   Entity,
-  JoinTable,
+  JoinColumn,
   ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
@@ -18,20 +18,12 @@ export class Tag extends CommonEntity {
   @Column('varchar', { name: 'name', length: 100 })
   name: string;
 
+  // user는 tags속성을 typeorm에서 갖고 db는 column이 없는게 맞다.
   @ManyToOne(() => User, (user) => user.tags)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'userId' })
   user: User;
 
   @ManyToMany(() => Context, (context) => context.tags)
-  @JoinTable({
-    name: 'context_tags',
-    joinColumn: {
-      name: 'tag_id',
-      referencedColumnName: 'tagId',
-    },
-    inverseJoinColumn: {
-      name: 'context_id',
-      referencedColumnName: 'contextId',
-    },
-  })
   contexts: Context[];
 }
+// @JoinColumn은 fk의 column명을 지정하고 typeorm상 참조할 컬럼명을 지정한다.
